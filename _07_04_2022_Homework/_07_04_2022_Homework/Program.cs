6dcfb73c1fab3a4fc6a98b83a3e44c0cfbdceda6
ElshadAhmadoff <744s72c@code.edu.az>
2022-04-07 05:49:51 +0400

    homework

---
+++ _07_04_2022_Homework/_07_04_2022_Homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using _07_04_2022_Homework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_04_2022_Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<char> letters = new MyList<char>();
+            letters.AddItem('a');
+            letters.AddItem('b'); 
+            letters.AddItem('c');
+            letters.AddItem('d');
+            letters.AddItem('f');
+            letters.AddItem('g');
+            for (int i = 0; i < letters._count; i++) Console.Write($"{letters[i]} ");
+            Console.ReadLine();
+        }
+    }
+}
